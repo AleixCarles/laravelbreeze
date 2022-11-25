a091fbbecbc7835c88a3984e0e3dd87e37b8602e
Aleix Carles <aleixcarles@iesebre.com>
2022-11-25 17:06:05 +0100

    video autentificacio laravel finalitzat

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('prova')" :active="request()->routeIs('prova')">
+                        {{ __('Prova') }}
+                    </x-nav-link>
                 </div>
             </div>
 
